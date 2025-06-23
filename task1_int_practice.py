f4c0c18f1cf3d1ca32fc15b5c30380908cf72cb8
Erkinov Sardor <you@example.com>
2025-06-23 12:06:14 +0500

    uyga vazifa qilindi23

--- task1_int_practice.py
+++ task1_int_practice.py
@@ -5,9 +5,11 @@
 # 3. Print both results
 
 # TODO: Convert string "5" to integer
-num1 = None  # Replace None with your code
+num1 = int("5")  # Replace None with your code
 
 # TODO: Convert string "42" to integer
-num2 = None  # Replace None with your code
+num2 = int("42")  # Replace None with your code
 
 # TODO: Print both numbers and verify they are integers using type()
+print(type(num1))
+print(type(num2))
