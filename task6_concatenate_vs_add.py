c1d44a073d229768ce8ed950ed895a847549db2d
Erkinov Sardor <you@example.com>
2025-06-24 20:45:34 +0500

    uyga vazifa qilindi

--- task6_concatenate_vs_add.py
+++ task6_concatenate_vs_add.py
@@ -5,9 +5,10 @@
 # 3. Compare the results
 
 # TODO: Concatenate strings "2" and "3"
-str_result = None  # Replace None with your code
+str_result = int("2")  # Replace None with your code
 
 # TODO: Convert strings to integers and add them
-int_result = None  # Replace None with your code
+int_result = int("3") # Replace None with your code
 
 # TODO: Print both results (concatenation and addition) and their types
+print(str_result+int_result)
